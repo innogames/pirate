bf96d9f966533fe3f29a1c5a8cfd1db91e4ad934
Norman Soetbeer <norman.soetbeer@innogames.de>
2017-02-24 18:22:02 +0100

    fixed tcp reconnect

--- pirate/writer.go
+++ pirate/writer.go
@@ -9,7 +9,9 @@ import (
 	"net/url"
 	"os"
 	"os/signal"
+	"sync"
 	"syscall"
+	"time"
 )
 
 func NewWriter(target string, logger *logging.Logger) (MetricWriter, error) {
@@ -59,27 +61,37 @@ func NewFileWriter(filename string, logger *logging.Logger) (*wrappedWriter, err
 		}
 	}()
 
-	return &wrappedWriter{file, reopen, logger}, nil
+	return &wrappedWriter{writer: file, reopen: reopen, logger: logger}, nil
 }
 
 func NewTcpWriter(addr string, logger *logging.Logger) (*wrappedWriter, error) {
-
 	conn, err := net.Dial("tcp", addr)
 	if err != nil {
 		return nil, fmt.Errorf("TCP error: %s", err)
 	}
 
-	reopen := func() error {
+	writer := &wrappedWriter{writer: conn, logger: logger}
+	writer.reopen = func() error {
 		conn.Close()
 
-		if conn, err = net.Dial("tcp", addr); err != nil {
-			return fmt.Errorf("TCP error: %s", err)
+		var newWriter net.Conn
+
+		for {
+			if newWriter, err = net.Dial("tcp", addr); err == nil {
+				break
+			}
+
+			logger.Debug("[TCP Writer] Failed to reconnect, trying again in 500 ms")
+			time.Sleep(500 * time.Millisecond)
 		}
+
+		writer.writer = newWriter
+		logger.Info("[TCP Writer] Reconnected successfully")
 
 		return nil
 	}
 
-	return &wrappedWriter{conn, reopen, logger}, nil
+	return writer, nil
 }
 
 type MetricWriter interface {
@@ -91,6 +103,7 @@ type wrappedWriter struct {
 	writer io.Writer
 	reopen func() error
 	logger *logging.Logger
+	mu     sync.Mutex
 }
 
 func (w *wrappedWriter) Write(m *Metric) error {
@@ -101,14 +114,21 @@ func (w *wrappedWriter) WriteRaw(path []byte, value []byte, timestamp []byte) er
 	buf := bytes.Join([][]byte{path, []byte(" "), value, []byte(" "), timestamp, []byte("\n")}, []byte{})
 
 	w.logger.Debugf("[Writer] Writing: %s", buf)
-	if _, err := w.writer.Write(buf); err != nil {
-		w.logger.Warningf("[Writer] Failed to write metric, trying to reopen")
-		if err = w.reopen(); err != nil {
-			return err
-		}
 
+	if _, err := w.writer.Write(buf); err != nil {
+		w.mu.Lock()
+		defer w.mu.Unlock()
+
+		// try to write again within the lock (it might be, that another goroutine already reconnected successfully)
 		if _, err := w.writer.Write(buf); err != nil {
-			return fmt.Errorf("[Metric Writer] Failed to write metric: %s", err)
+			w.logger.Warningf("[Writer] Failed to write metric, trying to reopen")
+			if err = w.reopen(); err != nil {
+				return err
+			}
+
+			if _, err := w.writer.Write(buf); err != nil {
+				return fmt.Errorf("[Metric Writer] Failed to write metric: %s", err)
+			}
 		}
 	}
 
